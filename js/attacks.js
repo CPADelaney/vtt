18914ada17831a9eab8a9e442543aeaca56d3643
CPADelaney <132380794+CPADelaney@users.noreply.github.com>
2024-12-10 16:28:08 -0800

    Update attacks.js

--- js/attacks.js
+++ js/attacks.js
@@ -1,5 +1,11 @@
 // js/attacks.js
 
+export const weapons = [
+  { id: 1, name: "Short Sword", damageDice: "1d6", stat: "STR", baseMod: 2 },
+  { id: 2, name: "Long Bow", damageDice: "1d8", stat: "DEX", baseMod: 1 },
+  { id: 3, name: "Dagger", damageDice: "1d4", stat: "DEX", baseMod: 3 }
+];
+
 export const attacksData = {
   1: {
     id: 1,
