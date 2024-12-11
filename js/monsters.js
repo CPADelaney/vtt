e367ae6e42bcff4bc5752e7faedebb415ab604ad
CPADelaney <132380794+CPADelaney@users.noreply.github.com>
2024-12-10 23:56:29 -0800

    Update monsters.js

--- js/monsters.js
+++ js/monsters.js
@@ -9,7 +9,7 @@ export const initialMonsters = [
     name: "Sand Wurm",
     HP: 50, AC: 14, STR: 18, DEX: 8, CON: 16, INT: 5, WIS: 10, CHA: 5,
     habitats: ["Desert"],
-    attacks: [{ attackId: 1 }] // Short Sword Strike (just a placeholder to test)
+    attacks: [{  }] // Short Sword Strike (just a placeholder to test)
   },
   {
     id: 2,
