9b83b9b0e6a91c5e91d2c591dec52fbb4c478f83
CPADelaney <132380794+CPADelaney@users.noreply.github.com>
2024-12-10 23:58:19 -0800

    Update monsters.js

--- js/monsters.js
+++ js/monsters.js
@@ -16,14 +16,14 @@ export const initialMonsters = [
     name: "Cactus Crawler",
     HP: 20, AC: 12, STR: 12, DEX: 14, CON: 10, INT: 2, WIS: 8, CHA: 6,
     habitats: ["Desert", "Grasslands"],
-    attacks: [{ attackId: 2 }] // Fireball (aoe)
+    attacks: [{ attackId: "2" }] // Fireball (aoe)
   },
   {
     id: 3,
     name: "River Drake",
     HP: 30, AC: 15, STR: 14, DEX: 12, CON: 13, INT: 6, WIS: 10, CHA: 8,
     habitats: ["Riverlands"],
-    attacks: [{ attackId: 3 }] // Dragon's Acid Breath (cone aoe)
+    attacks: [{ attackId: "3" }] // Dragon's Acid Breath (cone aoe)
   },
   {
     id: 4,
