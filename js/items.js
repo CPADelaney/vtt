18befd3549b9cded9621b5f2172a485d63d10693
CPADelaney <132380794+CPADelaney@users.noreply.github.com>
2024-12-10 16:38:10 -0800

    Update items.js

--- js/items.js
+++ js/items.js
@@ -1,1 +1,6 @@
-
+// items.js
+export const weapons = [
+  { id: 1, name: "Short Sword", damageDice: "1d6", stat: "STR", baseMod: 2 },
+  { id: 2, name: "Long Bow", damageDice: "1d8", stat: "DEX", baseMod: 1 },
+  { id: 3, name: "Dagger", damageDice: "1d4", stat: "DEX", baseMod: 3 }
+];
