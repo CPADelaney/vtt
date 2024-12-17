efe80ff34a5874360e27ee6d9568acf16fd95342
CPADelaney <132380794+CPADelaney@users.noreply.github.com>
2024-12-16 20:08:31 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,9 +4,3 @@ document.addEventListener('DOMContentLoaded', () => {
   const app = new App();
   app.initialize();
 });
-import { App } from './app.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-    const app = new App();
-    app.initialize();
-});
