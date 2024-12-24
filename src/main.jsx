809cd98d31c7088e703617481733f941d628ca11
CPADelaney <132380794+CPADelaney@users.noreply.github.com>
2024-12-23 16:44:23 -0800

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import VirtualTabletop from './components/VirtualTabletop.jsx' 
+// or wherever your main is
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <VirtualTabletop />
+)
