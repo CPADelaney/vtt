ca9b53b67894afbc95c85c35f539a1c74975789d
CPADelaney <132380794+CPADelaney@users.noreply.github.com>
2024-12-24 22:43:53 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import VirtualTabletop from './components/VirtualTabletop';
 import ToolsBar from './components/ToolsBar';     // We'll create ToolsBar.jsx
 import Sidebar from './components/Sidebar';       // or Chat/DM tools
-import './styles.css';
+import '../css/styles.css';
 
 export default function App() {
   return (
