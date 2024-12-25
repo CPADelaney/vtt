c3abe85104bc653bb366ccc71065f408d784c891
CPADelaney <132380794+CPADelaney@users.noreply.github.com>
2024-12-24 21:44:07 -0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import VirtualTabletop from './components/VirtualTabletop.jsx' 
-// or wherever your main is
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <VirtualTabletop />
-)
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
