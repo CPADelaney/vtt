e462b92406f95fae5eba0ec68d645f1425d729d1
CPADelaney <132380794+CPADelaney@users.noreply.github.com>
2024-12-24 16:26:56 -0800

    Update ZoomableContainer.jsx

--- src/components/ZoomableContainer.jsx
+++ src/components/ZoomableContainer.jsx
@@ -16,8 +16,8 @@ export function ZoomableContainer({
 
   // The parent container styles
   const containerStyle = {
-    width: '100%',
-    height: '100%',
+    width: '100vw',
+    height: '100vh',
     overflow: 'hidden',
     position: 'relative',
   };
