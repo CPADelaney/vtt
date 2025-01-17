bc158cd297f03db223eacab1fca1d852330d72ce
CPADelaney <132380794+CPADelaney@users.noreply.github.com>
2025-01-17 15:10:04 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 // src/App.jsx
 import React from 'react';
 import VirtualTabletop from './components/VirtualTabletop';
-import ToolsBar from './components/ToolsBar';     // We'll create ToolsBar.jsx
-import { Sidebar } from './components/Sidebar';
+import ToolsBar from './components/ToolsBar';
 import '../css/styles.css';
 
 export default function App() {
@@ -12,15 +11,13 @@ export default function App() {
       <div className="tools-bar">
         <ToolsBar />
       </div>
-
       {/* MIDDLE COLUMN (game area): flexible */}
       <div className="main-content">
         <VirtualTabletop />
       </div>
-
-      {/* RIGHT COLUMN (chat, DM Tools): 350px wide */}
+      {/* RIGHT COLUMN (chat): 350px wide */}
       <div className="right-sidebar">
-        <Sidebar />
+        <ChatBox />
       </div>
     </div>
   );
